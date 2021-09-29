68d89a95623952ac9b1af06acae6f5f28038d345
Nicholas Nzovia <nichonzovia@gmail.com>
2021-09-29 07:21:30 -0400

    Core C Programming, Operators and User Input

--- UserInput.c
+++ UserInput.c
@@ -2,15 +2,13 @@
 int main(){
     int a, b ,c;
     c =  a+b;
-        printf("C is %d",c);
-
-    // printf("Enter a: \n");
-    // scanf("%d", &a);
-    // printf("Enter b: \n");
-    // scanf("%d", &b);
-    // printf("a is %d \n",a);
-    // printf("b is %d \n",b);
-    // printf("Sum is %d \n",c);
+    printf("Enter a: \n");
+    scanf("%d", &a);
+    printf("Enter b: \n");
+    scanf("%d", &b);
+    printf("a is %d \n",a);
+    printf("b is %d \n",b);
+    printf("Sum is %d \n",c );
     return 0;
 
     
