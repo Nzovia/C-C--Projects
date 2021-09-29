c07a1f8abfa611dfebab5598b414442e411bfcc1
Nicholas Nzovia <nichonzovia@gmail.com>
2021-09-29 08:20:58 -0400

    escape sequences

---
+++ EscapeSequences.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    printf("Hello GoodBye");
+    printf("GoodBye \a");
+    printf("Hello \b");
+    printf("Why \t");
+    printf("Hello GoodBye \n");
+    printf("Hello\"GoodBye");
+
+    return 0;
+}
