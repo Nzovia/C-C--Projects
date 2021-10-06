bf65bb31380e5c9f1d55ebdafa08dd10180ad50b
Nicholas Nzovia <nichonzovia@gmail.com>
2021-10-06 05:45:12 -0400

    passing arrays in a function

--- Arrays.c
+++ Arrays.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main(){
+
+  //the function
+  void passArrayage(int number1, int number2){
+    printf("%d ", number1);
+    printf("%d ", number2);
+
+  }
     int array1[3][3]= {{23, 33, 43}, {53, 63,73},{35,45,55}};
       printf("multidimenational array list \n");
     for( int a=0; a<3; ++a){
@@ -15,13 +22,19 @@ int main(){
     for(int a = 0; a<6; a++){
         printf("%d ",array2[a]);
         if(array2[0]==2){
-          printf("leave);
+          printf("leave \n");
           break;
+
         }else{
           continue;
         }
         
     }
+       printf(".................Passing Array as a function...................\n");
+       int numbersArray[] ={22, 23, 24, 25};
+       
+       passArrayage(numbersArray[2], numbersArray[3]);
+
   
     return 0; 
 }
